ef1ea3450943a903d3ab5ce2d1aa87ad7a332297
Hiroki Kondo <kondo.matr02@gmail.com>
2020-01-14 19:22:10 +0900

    UIButtonをimageではなく、テキストで表示できるようにした。

--- ForButton/ViewController.swift
+++ ForButton/ViewController.swift
@@ -39,14 +39,14 @@ class ViewController: UIViewController {
             case 0,1,2:
                 imageButton.tag = i
                 imageButton.frame = CGRect(x: CGFloat(i) * self.view.frame.size.width/3, y: self.view.frame.size.width/3 * 1, width: self.view.frame.width/3, height: self.view.frame.size.width/3)
-                imageButton.setImage(UIImage(named: "\(i).jpg"), for: .normal)
+                imageButton.setTitle("\(i)", for: .normal)
                 imageButton.addTarget(self, action: #selector(tapButton(_ :)), for: .touchUpInside)
                 break
                 
             case 3,4,5:
                 imageButton.tag = i
                 imageButton.frame = CGRect(x: CGFloat(i-3) * self.view.frame.size.width/3, y: self.view.frame.size.width/3 * 2, width: self.view.frame.width/3, height: self.view.frame.size.width/3)
-                imageButton.setImage(UIImage(named: "\(i).jpg"), for: .normal)
+                imageButton.setTitle("\(i)", for: .normal)
                 imageButton.addTarget(self, action: #selector(tapButton(_ :)), for: .touchUpInside)
                 break
                 
@@ -54,12 +54,13 @@ class ViewController: UIViewController {
             case 6,7,8:
                 imageButton.tag = i
                 imageButton.frame = CGRect(x: CGFloat(i-6) * self.view.frame.size.width/3, y: self.view.frame.size.width/3 * 3, width: self.view.frame.width/3, height: self.view.frame.size.width/3)
-                imageButton.setImage(UIImage(named: "\(i).jpg"), for: .normal)
+                imageButton.setTitle("\(i)", for: .normal)
                 imageButton.addTarget(self, action: #selector(tapButton(_ :)), for: .touchUpInside)
                 break
                 
             default:
                 break
+                
             }
             self.view.addSubview(imageButton)
         }
